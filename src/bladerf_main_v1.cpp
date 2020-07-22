1134677205a611902c593258c933637747be0047
Dave <davemers@iu.edu>
2020-07-22 14:22:19 -0400

    adding code files

---
+++ src/bladerf_main_v1.cpp
@@ -0,0 +1,183 @@
+// ----------------------------------------------------------------------------------------
+#define _CRT_SECURE_NO_WARNINGS
+
+#if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
+
+#elif defined(__linux__)
+
+#endif
+
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+
+// bladeRF includes
+#include <libbladeRF.h>
+#include <bladeRF2.h>
+
+// ArrayFire Includes
+//#include <arrayfire.h>
+
+// Custom Includes
+#include "num2string.h"
+#include "get_current_time.h"
+#include "file_parser.h"
+#include "file_ops.h"
+
+// Project Includes
+
+// ----------------------------------------------------------------------------
+inline std::ostream& operator<< (
+    std::ostream& out,
+    const struct bladerf_devinfo &item
+    )
+{
+    out << "BladeRF Device Information: " << std::endl;
+    out << "  backend:       " << std::string(bladerf_backend_str(item.backend)) << std::endl;
+    out << "  serial number: " << std::string(item.serial) << std::endl;
+    out << "  usb_bus:       " << (uint32_t)item.usb_bus << std::endl;
+    out << "  usb_addr:      " << (uint32_t)item.usb_addr << std::endl;
+    out << "  instance:      " << item.instance << std::endl;
+    out << "  manufacturer:  " << std::string(item.manufacturer) << std::endl;
+    out << "  product:       " << std::string(item.product) << std::endl;
+    out << std::endl;
+    return out;
+}
+
+
+//-----------------------------------------------------------------------------
+int select_bladerf(int num_devices, struct bladerf_devinfo* device_list)
+{
+    uint32_t idx;
+    std::string console_input;
+
+    if (num_devices > 0)
+    {
+
+        for (idx = 0; idx < num_devices; ++idx)
+        {
+            std::cout << "BladeRF Device [" << idx << "]: " << std::string(device_list[idx].serial) << std::endl;
+            //std::cout << device_list[idx] << std::endl;
+        }
+        
+        std::cout << "Select BladeRF device number: ";
+        std::getline(std::cin, console_input);
+        return std::stoi(console_input);
+    }
+    else
+    {
+        std::cout << "Could not detect any bladeRF devices.  Check connections and try again..." << std::endl;
+    }
+
+    return -1;
+
+}   // end of get_device_list
+
+// ----------------------------------------------------------------------------
+void bladerf_status(int status)
+{
+    if (status != 0)
+    {
+        std::cout << "Unable to open device: " << std::string(bladerf_strerror(status)) << std::endl;
+        exit(status);
+    }
+    //return status;
+
+}
+
+// ----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+
+    uint32_t idx;
+    
+    // bladeRF variable
+    struct bladerf_devinfo *device_list = NULL;
+    struct bladerf_devinfo dev_info;
+    struct bladerf* dev;
+    int bladerf_num;
+    int blade_status;
+    bladerf_channel rx = BLADERF_CHANNEL_RX(0);
+    bladerf_channel tx = BLADERF_CHANNEL_TX(0);
+    bladerf_frequency rx_freq = 315000000;
+    bladerf_sample_rate sample_rate = 1000000;
+    bladerf_bandwidth rx_bw = 1000000;
+
+    std::vector<int16_t> samples;
+    uint32_t num_samples = 8192;
+    uint32_t timeout_ms = 10000;
+
+    int num_devices = bladerf_get_device_list(&device_list);
+
+    bladerf_num = select_bladerf(num_devices, device_list);
+
+    if (bladerf_num < 0)
+    {
+        std::cout << "could not detect any bladeRF devices..." << std::endl;
+        return 0;
+    }
+
+
+
+
+
+    try{
+        
+        blade_status = bladerf_open(&dev, ("*:serial=" +  std::string(device_list[bladerf_num].serial)).c_str());
+        if (blade_status != 0)
+        {
+            std::cout << "Unable to open device: " << std::string(bladerf_strerror(blade_status)) << std::endl;
+            return blade_status;
+        }
+
+        blade_status = bladerf_get_devinfo(dev, &dev_info);
+        if (blade_status != 0)
+        {
+            std::cout << "Unable to get the device info: " << std::string(bladerf_strerror(blade_status)) << std::endl;
+            return blade_status;
+        }
+        std::cout << std::endl << dev_info << std::endl;
+
+        // set the frequency, sample_rate and bandwidth
+        blade_status = bladerf_set_frequency(dev, rx, rx_freq);
+        blade_status = bladerf_set_sample_rate(dev, rx, sample_rate, &sample_rate);
+        blade_status = bladerf_set_bandwidth(dev, rx, rx_bw, &rx_bw);
+
+        // configure the sync to receive/transmit data
+        blade_status = bladerf_sync_config(dev, BLADERF_RX_X1, BLADERF_FORMAT_SC16_Q11, 10, 4096, 8, timeout_ms);
+
+        // enable the rx channel RF frontend
+        blade_status = bladerf_enable_module(dev, BLADERF_RX, true);
+
+        // receive the samples 
+        // the x2 is because one sample consists of one I and one Q.  The data should be packed IQIQIQIQIQIQ...
+        samples.resize(num_samples*2);
+        blade_status = bladerf_sync_rx(dev, (void*)samples.data(), num_samples, NULL, timeout_ms);
+        if (blade_status != 0)
+        {
+            std::cout << "Unable to get the required number of samples: " << std::string(bladerf_strerror(blade_status)) << std::endl;
+            return blade_status;
+        }
+
+        // print out the samples
+        std::cout << std::endl;
+        for (idx = 0; idx < samples.size(); idx+=2)
+        {
+            std::cout << samples[idx] << ", " << samples[idx + 1] << std::endl;
+        }
+
+        // disable the rx channel RF frontend
+        blade_status = bladerf_enable_module(dev, BLADERF_RX, false);
+
+        bladerf_close(dev);
+    }
+    catch(std::exception e)
+    {
+        std::cout << "error: " << e.what() << std::endl;
+    }
+    
+    int bp = 1;
+    
+    return 0;
+    
+}   // end of main
