79b77ebbd9f3fc68ed8c76a1d929ae8033386eb5
Dave <davemers@iu.edu>
2020-09-21 20:02:17 -0400

    getting the example running a little better

--- src/bladerf_main_v1.cpp
+++ src/bladerf_main_v1.cpp
@@ -100,12 +100,13 @@ int main(int argc, char** argv)
     int blade_status;
     bladerf_channel rx = BLADERF_CHANNEL_RX(0);
     bladerf_channel tx = BLADERF_CHANNEL_TX(0);
-    bladerf_frequency rx_freq = 315000000;
-    bladerf_sample_rate sample_rate = 1000000;
-    bladerf_bandwidth rx_bw = 1000000;
+    bladerf_frequency rx_freq = 314300000;
+    bladerf_sample_rate sample_rate = 2000000;
+    bladerf_bandwidth rx_bw = 5000000;
+    bladerf_gain rx1_gain = 10;
 
     std::vector<int16_t> samples;
-    uint32_t num_samples = 8192;
+    uint32_t num_samples = 131072;
     uint32_t timeout_ms = 10000;
 
     int num_devices = bladerf_get_device_list(&device_list);
@@ -118,10 +119,12 @@ int main(int argc, char** argv)
         return 0;
     }
 
+    std::cout << std::endl;
 
     af::setBackend(AF_BACKEND_CPU);
     af::info();
 
+    std::cout << std::endl;
 
     try{
         
@@ -145,6 +148,10 @@ int main(int argc, char** argv)
         blade_status = bladerf_set_sample_rate(dev, rx, sample_rate, &sample_rate);
         blade_status = bladerf_set_bandwidth(dev, rx, rx_bw, &rx_bw);
 
+        // the gain 
+        blade_status = bladerf_set_gain_mode(dev, rx, BLADERF_GAIN_MANUAL);
+        blade_status = bladerf_set_gain(dev, rx, rx1_gain);
+
         // configure the sync to receive/transmit data
         blade_status = bladerf_sync_config(dev, BLADERF_RX_X1, BLADERF_FORMAT_SC16_Q11, 10, 4096, 8, timeout_ms);
 
@@ -154,37 +161,58 @@ int main(int argc, char** argv)
         // receive the samples 
         // the x2 is because one sample consists of one I and one Q.  The data should be packed IQIQIQIQIQIQ...
         samples.resize(num_samples*2);
-        blade_status = bladerf_sync_rx(dev, (void*)samples.data(), num_samples, NULL, timeout_ms);
-        if (blade_status != 0)
-        {
-            std::cout << "Unable to get the required number of samples: " << std::string(bladerf_strerror(blade_status)) << std::endl;
-            return blade_status;
-        }
-
-        // try to compute the fft using the arrayfire library
-        std::vector<std::complex<float>> c_samples(num_samples);
-        int index = 0;
-        for (idx = 0; idx < samples.size(); idx += 2)
-        {
-            c_samples[index++] = std::complex<float>((float)samples[idx], (float)samples[idx+1]);
-        }
-        
-        af::array data(num_samples, (af::cfloat *)c_samples.data());
-
-        af::fftInPlace(data, 1.0/(double)num_samples);
-
-        af::array data2 = 2*af::log10(af::shift(af::abs(data), num_samples > 1));
-
-        auto d2 = data2.host<float>();
-
 
         af::Window myWindow(800, 800, "FFT example: ArrayFire");
-        af::array X = af::seq(0, num_samples-1, 1);
+        double freq_step = (sample_rate/1.0e6)/(double)num_samples;
 
+        float f_min = (rx_freq/1.0e6) - (sample_rate/2.0e6);
+        float f_max = (rx_freq / 1.0e6) + (sample_rate/2.0e6);
 
-        myWindow.plot(X, data2);
+        af::array X = af::seq(0, num_samples - 1, 1);
 
-        myWindow.show();
+        af::array f = af::seq(f_min, f_max - freq_step, freq_step);
+        af::array p = af::seq(-120, 20, 10);
+        auto p2 = p.dims(0);
+        auto f2 = f.dims(0);
+
+        myWindow.setAxesLimits(f, p, true);
+        myWindow.setAxesTitles("Frequency (MHz)", "Power (dBm)");
+
+        af::array raw_data, fft_data;
+
+        double scale = 1.0 / (double)(num_samples*50);
+
+        while (!myWindow.close())
+        {
+            blade_status = bladerf_sync_rx(dev, (void*)samples.data(), num_samples, NULL, timeout_ms);
+            if (blade_status != 0)
+            {
+                std::cout << "Unable to get the required number of samples: " << std::string(bladerf_strerror(blade_status)) << std::endl;
+                return blade_status;
+            }
+
+            // try to compute the fft using the arrayfire library
+            std::vector<std::complex<float>> c_samples(num_samples);
+            int index = 0;
+            for (idx = 0; idx < samples.size(); idx += 2)
+            {
+                c_samples[index++] = std::complex<float>((float)samples[idx], (float)samples[idx + 1]);
+            }
+
+            raw_data = af::array(num_samples, (af::cfloat*)c_samples.data());
+            //auto d1 = data.host<af::cfloat>();
+
+            af::fftInPlace(raw_data, scale);
+
+            fft_data = 20 * af::log10(af::shift(af::abs(raw_data), (num_samples >> 1)))-40;
+
+            //auto d2 = data2.host<float>();
+
+            // show the results of the FFT in the window
+            myWindow.plot(f, fft_data);
+            myWindow.show();
+
+        }
 
         // print out the samples
         //std::cout << std::endl;
@@ -192,7 +220,7 @@ int main(int argc, char** argv)
         //{
         //    std::cout << samples[idx] << ", " << samples[idx + 1] << std::endl;
         //}
-        std::cin.ignore();
+        //std::cin.ignore();
 
         
 
@@ -208,6 +236,7 @@ int main(int argc, char** argv)
     catch (af::exception e)
     {
         std::cout << "error: " << e.what() << std::endl;
+        std::cin.ignore();
     }
     
     int bp = 1;
