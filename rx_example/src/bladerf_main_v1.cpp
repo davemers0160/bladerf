b51ff3f2645122c915018bf46e713f86131633a8
Dave <davemers@iu.edu>
2020-10-14 20:50:20 -0400

    working on FFT display

--- rx_example/src/bladerf_main_v1.cpp
+++ rx_example/src/bladerf_main_v1.cpp
@@ -47,45 +47,6 @@
 //}
 //
 //
-////-----------------------------------------------------------------------------
-//int select_bladerf(int num_devices, struct bladerf_devinfo* device_list)
-//{
-//    uint32_t idx;
-//    std::string console_input;
-//
-//    if (num_devices > 0)
-//    {
-//
-//        for (idx = 0; idx < num_devices; ++idx)
-//        {
-//            std::cout << "BladeRF Device [" << idx << "]: " << std::string(device_list[idx].serial) << std::endl;
-//            //std::cout << device_list[idx] << std::endl;
-//        }
-//        
-//        std::cout << "Select BladeRF device number: ";
-//        std::getline(std::cin, console_input);
-//        return std::stoi(console_input);
-//    }
-//    else
-//    {
-//        std::cout << "Could not detect any bladeRF devices.  Check connections and try again..." << std::endl;
-//    }
-//
-//    return -1;
-//
-//}   // end of get_device_list
-//
-//// ----------------------------------------------------------------------------
-//void bladerf_status(int status)
-//{
-//    if (status != 0)
-//    {
-//        std::cout << "Unable to open device: " << std::string(bladerf_strerror(status)) << std::endl;
-//        exit(status);
-//    }
-//    //return status;
-//
-//}
 
 // ----------------------------------------------------------------------------
 int main(int argc, char** argv)
@@ -102,13 +63,22 @@ int main(int argc, char** argv)
     bladerf_channel rx = BLADERF_CHANNEL_RX(0);
     bladerf_channel tx = BLADERF_CHANNEL_TX(0);
     bladerf_frequency rx_freq = 314300000;
-    bladerf_sample_rate sample_rate = 2000000;
-    bladerf_bandwidth rx_bw = 5000000;
+    bladerf_sample_rate sample_rate = 5000000;
+    bladerf_bandwidth rx_bw = 1000000;
     bladerf_gain rx1_gain = 10;
+    int64_t span = 1000000;
 
     std::vector<int16_t> samples;
-    uint32_t num_samples = 65536;
+    uint32_t num_samples = 65536*2;
     uint32_t timeout_ms = 10000;
+    const uint32_t num_buffers = 16;
+    const uint32_t buffer_size = 1024 * 8;        // must be a multiple of 1024
+    const uint32_t num_transfers = 8;
+
+#ifdef USE_ARRAYFIRE
+    // array fire variables
+    af::array raw_data, fft_data;
+#endif // USE_ARRAYFIRE
 
     int num_devices = bladerf_get_device_list(&device_list);
 
@@ -123,12 +93,7 @@ int main(int argc, char** argv)
     std::cout << std::endl;
 
     try{
-
-        af::setBackend(AF_BACKEND_CPU);
-        af::info();
-
-        std::cout << std::endl;
-        
+      
         blade_status = bladerf_open(&dev, ("*:serial=" +  std::string(device_list[bladerf_num].serial)).c_str());
         if (blade_status != 0)
         {
@@ -154,36 +119,49 @@ int main(int argc, char** argv)
         blade_status = bladerf_set_gain(dev, rx, rx1_gain);
 
         // configure the sync to receive/transmit data
-        blade_status = bladerf_sync_config(dev, BLADERF_RX_X1, BLADERF_FORMAT_SC16_Q11, 10, 4096, 8, timeout_ms);
+        blade_status = bladerf_sync_config(dev, BLADERF_RX_X1, BLADERF_FORMAT_SC16_Q11, num_buffers, buffer_size, num_transfers, timeout_ms);
 
         // enable the rx channel RF frontend
         blade_status = bladerf_enable_module(dev, BLADERF_RX, true);
 
         // receive the samples 
-        // the x2 is because one sample consists of one I and one Q.  The data should be packed IQIQIQIQIQIQ...
+        // the *2 is because one sample consists of one I and one Q.  The data should be packed IQIQIQIQIQIQ...
         samples.resize(num_samples*2);
 
+        double freq_step = (sample_rate)/(double)num_samples;
+
+        double f_min = (rx_freq - (span>>1)) * 1.0e-6;
+        double f_max = (rx_freq + (span>>1)) * 1.0e-6;
+
+        uint32_t sp = (uint32_t)((sample_rate - span) / (2.0 * freq_step));
+        uint32_t sp2 = (uint32_t)(span / freq_step);
+
+        double scale = 1.0 / (double)(num_samples);
+
+#ifdef USE_ARRAYFIRE
+
+        af::setBackend(AF_BACKEND_CPU);
+        af::info();
+
+        std::cout << std::endl;
+
         af::Window myWindow(800, 800, "FFT example: ArrayFire");
-        double freq_step = (sample_rate/1.0e6)/(double)num_samples;
+        //af::array X = af::seq(0, num_samples - 1, 1);
+        af::array X = af::seq(sp+1, (sp+sp2), 1);
+        //auto x2d = X.dims(0);
 
-        double f_min = (rx_freq/1.0e6) - (sample_rate/2.0e6);
-        double f_max = (rx_freq / 1.0e6) + (sample_rate/2.0e6);
+        af::array f = af::seq(f_min, f_max - (freq_step*1.0e-6), (freq_step*1.0e-6));
 
-        af::array X = af::seq(0, num_samples - 1, 1);
-
-        af::array f = af::seq(f_min, f_max - freq_step, freq_step);
-        af::array p = af::seq(-120, 20, 10);
-        auto p2 = p.dims(0);
-        auto f2 = f.dims(0);
-
-        myWindow.setAxesLimits(f, p, true);
+        myWindow.setAxesLimits(f_min, f_max - (freq_step * 1.0e-6), -120, -20, true);
         myWindow.setAxesTitles("Frequency (MHz)", "Power (dBm)");
 
-        af::array raw_data, fft_data;
-
-        double scale = 1.0 / (double)(num_samples*50);
-
         while (!myWindow.close())
+
+#else
+
+        while(1)
+#endif
+
         {
             blade_status = bladerf_sync_rx(dev, (void*)samples.data(), num_samples, NULL, timeout_ms);
             if (blade_status != 0)
@@ -200,47 +178,39 @@ int main(int argc, char** argv)
                 c_samples[index++] = std::complex<float>((float)samples[idx], (float)samples[idx + 1]);
             }
 
-            raw_data = af::array(num_samples, (af::cfloat*)c_samples.data());
-            //auto d1 = data.host<af::cfloat>();
+#ifdef USE_ARRAYFIRE
+            raw_data = af::array(num_samples, (af::cfloat*)c_samples.data())*(1/2048.0);
 
             af::fftInPlace(raw_data, scale);
+            //auto a1 = af::abs(raw_data).host<float>();
 
-            fft_data = 20 * af::log10(af::shift(af::abs(raw_data), (num_samples >> 1)))-40;
-
-            //auto d2 = data2.host<float>();
+            fft_data = 20 * af::log10(af::shift(af::abs(raw_data), (num_samples >> 1)))-10;
 
             // show the results of the FFT in the window
-            myWindow.plot(f, fft_data);
+            myWindow.plot(f, fft_data(X));
             myWindow.show();
-
+#endif
         }
-
-        // print out the samples
-        //std::cout << std::endl;
-        //for (idx = 0; idx < samples.size(); idx+=2)
-        //{
-        //    std::cout << samples[idx] << ", " << samples[idx + 1] << std::endl;
-        //}
-        //std::cin.ignore();
-
-        
 
         // disable the rx channel RF frontend
         blade_status = bladerf_enable_module(dev, BLADERF_RX, false);
 
         bladerf_close(dev);
     }
-    //catch(std::exception e)
-    //{
-    //    std::cout << "error: " << e.what() << std::endl;
-    //}
+
+#ifdef USE_ARRAYFIRE
     catch (af::exception e)
     {
         std::cout << "error: " << e.what() << std::endl;
         std::cin.ignore();
     }
-    
-    int bp = 1;
+#else
+    catch(std::exception e)
+    {
+        std::cout << "error: " << e.what() << std::endl;
+    }
+#endif
+
     
     return 0;
     
